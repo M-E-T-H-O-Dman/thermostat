7e846ba3168800fde61eaf50a32aca835d82a289
Egalitea-leaves <charlie.redwood@gmail.com>
2014-08-26 15:43:36 +0100

    energyUsage added

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -109,5 +109,25 @@ describe("Thermostat", function() {
       expect(thermostat.temperature).toEqual(20)
     })
   })
+
+  describe('energy usage', function() {
+    it('is efficient if < 18', function() {
+      thermostat.temperature = 15;
+
+      expect(thermostat.energyUsage()).toEqual('efficient');
+    })
+
+    it('is average if > 18 and < 25', function() {
+      thermostat.temperature = 20;
+
+      expect(thermostat.energyUsage()).toEqual('average');
+    })
+
+    it('is inefficient if > 25', function() {
+      thermostat.temperature = 26;
+
+      expect(thermostat.energyUsage()).toEqual('inefficient');
+    })
+  })
 });
  
