991122e6c17f4c71d75282e865095d3777b364f0
Egalitea-leaves <charlie.redwood@gmail.com>
2014-08-26 13:36:35 +0100

    second commit

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,21 @@
+describe("Thermostat", function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat;
+  });
+
+  it("should begin at 20 degrees", function() {
+    expect(thermostat.temperature).toEqual(20);
+
+  });
+
+  describe('increasing the temperature', function() {
+    it('increases if < 25 degress', function() {
+      thermostat.increaseTemperature();
+      expect(thermostat.temperature).toEqual(21);
+    })
+  })
+});
+ 
